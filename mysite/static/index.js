384c81c5f866a015569f649e380097d0a35720d6
Pedrinhooo <50207991+Pedrinhooo@users.noreply.github.com>
2022-12-12 13:45:23 -0300

    5.8

--- mysite/static/index.js
+++ mysite/static/index.js
@@ -1,7 +1,6 @@
-
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-analytics.js";
+  import { getFirestore } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -20,4 +19,6 @@
 
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
+  const db = getFirestore(app);
+
+  console.log(app);
